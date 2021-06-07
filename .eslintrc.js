0589b4269e23b823778aaf58773959be8ca1e3c0
slowik <piotrslowik409@gmail.com>
2021-06-07 19:06:31 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  plugins :[
+     "react",
+     "react-native"
+  ],
+  parserOptions: {
+    "eacmaFeatures": {
+"jsx": true
+
+    }
+  },
+  "rules": {
+    "react-native/no-unused-styles": 2,
+    "react-native/split-platform-components": 2,
+    "react-native/no-inline-styles": 2,
+    "react-native/no-color-literals": 2,
+    "react-native/no-raw-text": 2,
+    "react-native/no-single-element-style-arrays": 2,
+  },
+
+};
